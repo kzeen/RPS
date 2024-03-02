1b2a1ee023219c8c96fe2e131227e1fe331d2688
zeeny <157370421+kzeen@users.noreply.github.com>
2024-03-03 00:09:13 +0200

    Implement getPlayerChoice functionality

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function getComputerChoice() {
     const computerChoices = ["Rock", "Paper", "Scissors"];
     return computerChoices[getRandomElement(computerChoices)];
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter your play:");
+    // Format user input to only capitalize first letter
+    playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
+    return playerChoice;
+}
